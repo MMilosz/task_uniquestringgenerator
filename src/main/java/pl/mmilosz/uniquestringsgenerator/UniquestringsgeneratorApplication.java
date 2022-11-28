089bb23728dc325e55ba1460930a41c46def26aa
MMilosz <webdev@mmilosz.pl>
2022-11-28 22:02:33 +0100

    Create project scaffold

---
+++ src/main/java/pl/mmilosz/uniquestringsgenerator/UniquestringsgeneratorApplication.java
@@ -0,0 +1,13 @@
+package pl.mmilosz.uniquestringsgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniquestringsgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniquestringsgeneratorApplication.class, args);
+	}
+
+}
